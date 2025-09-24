ee390bbc8b3582e575a1c586d14e9f0a514ec05f
Carlos Pinel <59676743+caduard0@users.noreply.github.com>
2025-09-24 01:39:07 -0300

    added a buch of files

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,3 +1,21 @@
 #!/bin/zsh
-
 # Make simlink to files
+# ln -s ~/.dotfiles/{dot_file} ~/{dot_file_path}
+
+# Files
+ln -fs ~/.dotfiles/.zshrc ~/.zshrc
+ln -fs ~/.dotfiles/.gitconfig ~/.gitconfig
+
+# Directories
+mkdir -p ~/.config/hypr
+ln -fs ~/.dotfiles/hypr/* ~/.config/hypr/
+mkdir -p ~/.config/kitty
+ln -fs ~/.dotfiles/kitty/* ~/.config/kitty/
+mkdir -p ~/.config/waybar
+ln -fs ~/.dotfiles/waybar/* ~/.config/waybar/
+mkdir -p ~/.config/gtk-3.0
+ln -fs ~/.dotfiles/gtk-3.0/* ~/.config/gtk-3.0/
+mkdir -p ~/.config/gtk-4.0
+ln -fs ~/.dotfiles/gtk-4.0/* ~/.config/gtk-4.0/
+mkdir -p ~/.config/xsettingsd
+ln -fs ~/.dotfiles/xsettingsd/* ~/.config/xsettingsd/
