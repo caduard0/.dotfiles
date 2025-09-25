0b22f5dfe6d05f858c454ad919b6c1f0ceda9249
Carlos Pinel <59676743+caduard0@users.noreply.github.com>
2025-09-24 23:10:18 -0300

    neovim packages

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 yay -S --needed swww waybar ttf-go-nerd hyprpicker power-profiles-daemon python-gobject nwg-look papirus-icon-theme papirus-folders wl-clipboard 
-yay -S --needed neovim llvm clang
+yay -S --needed neovim llvm clang cargo cmake pyright lua texlab lua-language-server
